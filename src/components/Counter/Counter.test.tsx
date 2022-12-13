c5951911f4c705b0a124581c87fb85f113a59ee2
kiratanaka0521 <kiratanaka0521@gmail.com>
2022-12-13 18:33:11 +0900

    [ feature ] counter

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -1,15 +1,22 @@
 import { fireEvent, render, screen } from "@testing-library/react"
 
-import { Counter } from "./"
+import { Counter } from "./Counter"
 
+// スナップショットテストは「変更検知」のために利用する
 describe("Counter", () => {
   test("render", () => {
-    const { asFragment } = render(<Counter />)
-    expect(asFragment()).toMatchSnapshot()
+    // const { asFragment } = render(<Counter />)
+    // expect(asFragment()).toMatchSnapshot()
+    const { getByText } = render(<Counter />)
+    getByText("Count: 0")
+
+    screen.debug(screen.getByText("Count: 0"))
   })
+
   test("click:count", () => {
     render(<Counter />)
     const button = screen.getByText("Increment")
+    screen.debug(button)
     fireEvent.click(button)
     fireEvent.click(button)
     screen.getByText("Count: 2")
